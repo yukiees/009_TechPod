3a36e9732f25b586807ce6602ca9610c39a3ea7c
yukiees <yukiii8989@gmail.com>
2020-11-19 12:28:49 +0900

    Initial Commit

---
+++ 009_TechPod/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  009_TechPod
+//
+//  Created by 松島優希 on 2020/11/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
